e932eac20e44996f0736fadc1b339361b07bb69f
valenty-nka <valenty.nechyporenko@gmail.com>
2024-08-20 20:41:11 -0230

    Solve sumAll test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+let sum = 0;
+if (num1 > num2) {
+    [num1, num2] = [num2, num1]
+}
+if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+    sum = "ERROR";
+} else{
+    for (let i = num1; i <= num2; i++){
+        sum += i;
+    }
+}
 
+return sum;
 };
+
+sumAll(2,4);
 
 // Do not edit below this line
 module.exports = sumAll;
