adb4d1ffbe04108e285a410404f166ba13fc1813
valenty-nka <valenty.nechyporenko@gmail.com>
2024-08-20 20:18:12 -0230

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    args = [...args];
+    let newArr = [];
+    console.log(args);
+    for (let i = 0; i < arr.length; i++){
+        if(!args.includes(arr[i])) {
+            newArr.push(arr[i]);
+        } 
+        
+    }
+    console.log(newArr)
+    return newArr;
+    
 };
-
+removeFromArray([1,2,3], 2)
 // Do not edit below this line
 module.exports = removeFromArray;
