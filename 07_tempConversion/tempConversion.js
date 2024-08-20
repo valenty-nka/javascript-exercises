f879a156d4fb9cb0fa555dd8452d27076f76a875
valenty-nka <valenty.nechyporenko@gmail.com>
2024-08-20 21:26:26 -0230

    solve tempConversion test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempInCelsius = (temp - 32) * 5/9;
+  return parseFloat(tempInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempInFahrenheit = temp * 9/5 + 32;
+  return parseFloat(tempInFahrenheit.toFixed(1));
 };
+
+convertToCelsius(100)
 
 // Do not edit below this line
 module.exports = {
