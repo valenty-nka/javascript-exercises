17007556b06af4351620fefb5da7f6ec010c9844
valenty-nka <valenty.nechyporenko@gmail.com>
2024-08-20 20:17:51 -0230

    Solve reverseString test js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let newStr = "";
+for (let i = str.length - 1; i >= 0; i--){
+    newStr += str[i];
+}
+return newStr;
 };
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
